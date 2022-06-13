4f843ed3de99993eca21b4b5ec45fa6d3bc97af3
vikharsneha <snehaborkar28@gmail.com>
2022-06-13 11:11:27 +0530

    first commit

---
+++ src/main/java/com/example/demo/EmployeeWithoutDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWithoutDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWithoutDbApplication.class, args);
+	}
+
+}
